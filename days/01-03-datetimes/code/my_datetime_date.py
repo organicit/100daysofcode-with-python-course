6dc3155f3ba2039e54de26fee34f41f8bb03f18d
Eric Courville <organicit-git@outlook.com>
2018-03-30 14:17:51 -0700

    created pomodoro function and pycon date function

---
+++ days/01-03-datetimes/code/my_datetime_date.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+from datetime import date
+import time
+import os
+
+
+def new_pomodoro():
+    total_pom_minutes = 25
+    total_pom_seconds = total_pom_minutes * 60
+    time.sleep(total_pom_seconds)
+
+
+def alexa_pomodoro():
+    os.system('say "Alexa, set timer for 25 minutes"')
+
+
+def is_it_pycon():
+    pycon_start_date = date(2018, 5, 19)
+    pycon_end_date = date(2018, 5, 17)
+    todaydate = date.today()
+
+    if pycon_start_date <= todaydate <= pycon_end_date:
+        print("it's Pycon2018 Baby!!!!")
+    else:
+        if todaydate > pycon_end_date:
+            print("You missed Pycon, try next year")
+        else:
+            print("There are still " + str((pycon_start_date - todaydate).days) + " days until Pycon2018")
+            print("I guess I will code in the meantime.  Let's do a pomodoro")
+            #new_pomodoro()
+            alexa_pomodoro()
+
+is_it_pycon()
